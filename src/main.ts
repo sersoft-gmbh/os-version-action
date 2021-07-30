c4c922edd94ac7ee36f94427b5f9b97a2bcb9eca
Florian Friedrich <ffried@me.com>
2021-07-30 10:26:43 +0200

    Adjust version lookup on Windows

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import * as core from '@actions/core';
 import { getExecOutput } from '@actions/exec';
+import { EOL } from 'os';
 
 async function runCmd(cmd: string, args?: string[]): Promise<string> {
     const output = await getExecOutput(cmd, args, {
@@ -20,9 +21,17 @@ async function main() {
             break;
         case 'win32':
         case 'cygwin':
-            const major = await runCmd('pwsh', ['-Command', '[System.Environment]::OSVersion.Version.Major']);
-            const minor = await runCmd('pwsh', ['-Command', '[System.Environment]::OSVersion.Version.Minor']);
-            version = `${major.trim()}.${minor.trim()}`;
+            const systemInfo = await runCmd('systeminfo');
+            const nameVersionRegex = /^OS Name:\s+[A-Za-z0-9 ]*([0-9.]+)[A-Za-z0-9 ]*$/;
+            const matchingLine = systemInfo.split(EOL).find(l => nameVersionRegex.test(l));
+            if (matchingLine) {
+                version = matchingLine.replace(nameVersionRegex, '$1');
+            } else {
+                core.warning('Could not find a suitable version in `systeminfo`. Falling back to `[System.Environment]::OSVersion`...');
+                const major = await runCmd('pwsh', ['-Command', '[System.Environment]::OSVersion.Version.Major']);
+                const minor = await runCmd('pwsh', ['-Command', '[System.Environment]::OSVersion.Version.Minor']);
+                version = `${major.trim()}.${minor.trim()}`;
+            }
             break;
         default:
             throw new Error('Unsupported platform: ' + process.platform);
