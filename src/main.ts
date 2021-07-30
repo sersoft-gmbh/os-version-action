36190b6c70745306fb30dacc01adbffcd1c6d464
Florian Friedrich <ffried@me.com>
2021-07-30 10:45:47 +0200

    Make regex greedy

--- src/main.ts
+++ src/main.ts
@@ -22,7 +22,7 @@ async function main() {
         case 'win32':
         case 'cygwin':
             const systemInfo = await runCmd('systeminfo');
-            const nameVersionRegex = /^OS Name:\s+[A-Za-z0-9 ]*([0-9.]+)[A-Za-z0-9 ]*$/;
+            const nameVersionRegex = /^OS Name:\s+[A-Za-z0-9 ]*?([0-9.]+)[A-Za-z0-9 ]*?$/;
             const matchingLine = systemInfo.split(EOL).find(l => nameVersionRegex.test(l));
             if (matchingLine) {
                 version = matchingLine.replace(nameVersionRegex, '$1');
